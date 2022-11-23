fac5be922d0dd3c933a7f40f9656b6f5a70d22c9
Pedro Simões <pedro.simoes@easypay.pt>
2022-11-23 12:57:47 +0000

    feat: support ping test to TestNewBalancer

--- balancer_test.go
+++ balancer_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/agiledragon/gomonkey/v2"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/stretchr/testify/assert"
@@ -15,8 +16,9 @@ func TestNewBalancer(t *testing.T) {
 	tests := []struct {
 		name    string
 		config  *Config
-		prepare func()
-		want    *Balancer
+		prepare func(*testing.T) (*sql.DB, sqlmock.Sqlmock, *gomonkey.Patches)
+		verify  func(*testing.T, sqlmock.Sqlmock)
+		want    assert.ValueAssertionFunc
 		wantErr assert.ErrorAssertionFunc
 	}{
 		{
@@ -26,7 +28,9 @@ func TestNewBalancer(t *testing.T) {
 				Strategy:   nil,
 				Connection: Connection{},
 			},
-			want:    nil,
+			prepare: nopPrepare,
+			verify:  nopVerify,
+			want:    assert.Nil,
 			wantErr: assert.Error,
 		},
 		{
@@ -39,7 +43,9 @@ func TestNewBalancer(t *testing.T) {
 				Strategy:   nil,
 				Connection: Connection{},
 			},
-			want:    nil,
+			prepare: nopPrepare,
+			verify:  nopVerify,
+			want:    assert.Nil,
 			wantErr: assert.Error,
 		},
 		{
@@ -57,7 +63,9 @@ func TestNewBalancer(t *testing.T) {
 					Driver:             "",
 				},
 			},
-			want:    nil,
+			prepare: nopPrepare,
+			verify:  nopVerify,
+			want:    assert.Nil,
 			wantErr: assert.Error,
 		},
 		{
@@ -75,7 +83,9 @@ func TestNewBalancer(t *testing.T) {
 					Driver:             "mysql",
 				},
 			},
-			want:    nil,
+			prepare: nopPrepare,
+			verify:  nopVerify,
+			want:    assert.Nil,
 			wantErr: assert.Error,
 		},
 		{
@@ -88,31 +98,120 @@ func TestNewBalancer(t *testing.T) {
 				Strategy: nil,
 				Connection: Connection{
 					MaxOpenConnections: 10,
-					MaxIdleConns:       0,
+					MaxIdleConns:       1,
 					ConnMaxLifetime:    1 * time.Hour,
 					Driver:             "mysql",
 				},
 			},
-			prepare: func() {
-				_ = gomonkey.ApplyMethodFunc(&sql.DB{}, "Ping", func() error {
-					return nil
+			prepare: func(t *testing.T) (*sql.DB, sqlmock.Sqlmock, *gomonkey.Patches) {
+				db, mock, err := sqlmock.New(sqlmock.MonitorPingsOption(true))
+				assert.NoError(t, err)
+
+				patch := gomonkey.ApplyFunc(sql.Open, func(driverName, dataSourceName string) (*sql.DB, error) {
+					return db, nil
 				})
-				//defer patch.Reset()
+
+				e := mock.ExpectPing()
+				e.WillReturnError(fmt.Errorf("could not ping"))
+				return db, mock, patch
 			},
-			want:    nil,
+			verify: func(t *testing.T, mock sqlmock.Sqlmock) {
+				assert.NoError(t, mock.ExpectationsWereMet())
+			},
+			want:    assert.Nil,
 			wantErr: assert.Error,
+		},
+		{
+			name: "it fails if strategyfunc returns an error",
+			config: &Config{
+				Nodes: Nodes{{
+					Address: "root:Mastermaster123@tcp(127.0.0.1:3306)/mysql",
+					Name:    "master",
+				}},
+				Strategy: func(Nodes) (Strategy, error) {
+					return nil, fmt.Errorf("failed to bootstrap strategy")
+				},
+				Connection: Connection{
+					MaxOpenConnections: 10,
+					MaxIdleConns:       1,
+					ConnMaxLifetime:    1 * time.Hour,
+					Driver:             "mysql",
+				},
+			},
+			prepare: func(t *testing.T) (*sql.DB, sqlmock.Sqlmock, *gomonkey.Patches) {
+				db, mock, err := sqlmock.New(sqlmock.MonitorPingsOption(true))
+				assert.NoError(t, err)
+
+				patch := gomonkey.ApplyFunc(sql.Open, func(driverName, dataSourceName string) (*sql.DB, error) {
+					return db, nil
+				})
+
+				mock.ExpectPing()
+				return db, mock, patch
+			},
+			verify: func(t *testing.T, mock sqlmock.Sqlmock) {
+				assert.NoError(t, mock.ExpectationsWereMet())
+			},
+			want:    assert.Nil,
+			wantErr: assert.Error,
+		},
+		{
+			name: "creates new balancer",
+			config: &Config{
+				Nodes: Nodes{{
+					Address: "root:Mastermaster123@tcp(127.0.0.1:3306)/mysql",
+					Name:    "master",
+				}},
+				Strategy: func(n Nodes) (Strategy, error) { return NewRoundRobinStrategy(n) },
+				Connection: Connection{
+					MaxOpenConnections: 10,
+					MaxIdleConns:       1,
+					ConnMaxLifetime:    1 * time.Hour,
+					Driver:             "mysql",
+				},
+			},
+			prepare: func(*testing.T) (*sql.DB, sqlmock.Sqlmock, *gomonkey.Patches) {
+				db, mock, err := sqlmock.New(sqlmock.MonitorPingsOption(true))
+				assert.NoError(t, err)
+
+				patch := gomonkey.ApplyFunc(sql.Open, func(driverName, dataSourceName string) (*sql.DB, error) {
+					return db, nil
+				})
+
+				e := mock.ExpectPing()
+				e.WillReturnError(nil)
+				return db, mock, patch
+			},
+			verify: func(t *testing.T, mock sqlmock.Sqlmock) {
+				assert.NoError(t, mock.ExpectationsWereMet())
+			},
+			want:    assert.NotNil,
+			wantErr: assert.NoError,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if tt.prepare != nil {
-				tt.prepare()
-			}
+			db, mock, patches := tt.prepare(t)
+			defer func() {
+				if db != nil {
+					db.Close()
+				}
+				if patches != nil {
+					patches.Reset()
+				}
+			}()
+
 			got, err := NewBalancer(tt.config)
-			if !tt.wantErr(t, err, fmt.Sprintf("NewBalancer(%v)", tt.config)) {
-				return
-			}
-			assert.Equalf(t, tt.want, got, "NewBalancer(%v)", tt.config)
+			tt.wantErr(t, err)
+			tt.want(t, got)
+			tt.verify(t, mock)
 		})
 	}
+}
+
+func nopPrepare(*testing.T) (*sql.DB, sqlmock.Sqlmock, *gomonkey.Patches) {
+	return nil, nil, nil
+}
+
+func nopVerify(*testing.T, sqlmock.Sqlmock) {
 }
